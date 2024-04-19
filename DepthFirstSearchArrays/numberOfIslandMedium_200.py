b63042ff0d95e4885c05b2698af3339710fbd497
sunnyallana <study.sunnyallana@gmail.com>
2024-04-19 10:13:37 +0500

    Added 200

---
+++ DepthFirstSearchArrays/numberOfIslandMedium_200.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        visited = set()
+
+        def cannotVisit(i, j):
+            return (i < 0 or i >= len(grid)) or (j < 0 or j >= len(grid[0])) or (grid[i][j] == "0") or ((i, j) in visited) 
+
+        def depthFirstSearch(i, j):
+            if cannotVisit(i, j):
+                return 0
+
+            visited.add((i, j))
+
+            return 1 + depthFirstSearch(i+1, j) + depthFirstSearch(i, j+1) + depthFirstSearch(i-1, j) + depthFirstSearch(i, j-1)  
+
+
+        count = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if(grid[i][j] == "1" and not cannotVisit(i,j)):
+                    if(depthFirstSearch(i, j)):
+                        count += 1
+        return count
